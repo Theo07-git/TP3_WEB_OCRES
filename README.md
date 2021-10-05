eaca0d4a308262fe1797a4beeaa18be1792014cf
theoperrier <theo.perrier@edu.ece.fr>
2021-10-05 19:09:34 +0200

    added the fourth functionnality with a for loop that modify the dom dynamically

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ Le bouton se sert donc plus à rien, il faut l'enlever.
 
 3. Par défault la ville choisi pour les prédictions météo est Paris. Faire en sorte que votre user puisse rentrer le nom d'une ville dans l'input. Lorsqu'il appuit sur **Actualiser** cela actualise les informations avec la bonne ville. Il faut donc faire une requete différente.
 *Hint: [Comment récupérer la valeur d'un input](https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascript)*
+######check
 
 4. Chercher les prévisions météo pour les trois jours à venir.
 *Hint: utiliser [la même API mais un end point différent](https://openweathermap.org/forecast16)* (cf: forecast/daily)
@@ -28,7 +29,10 @@ Le bouton se sert donc plus à rien, il faut l'enlever.
 * Utilisez les fonctions map, filter, sort pour manipuler la data.
 * Je vous conseille vivement d'afficher ce que retourne cette API afin de voir ou chercher l'information.
 * Afficher les mêmes informations que pour la météo d'aujourd'hui, a savoir : le temps principal 'main', la description, la température en Celsius, l'icon.
+######check
 
 BONUS (pour ceux qui veulent aller plus loin): à l'aide d'une boucle, modifier le DOM pour les 4 jours sans dédoubler le code pour chaque jour.
+######check
 
 5. Le code inital est responsive, faire en sorte qu'il le reste... (Si vous avez tout bien fait, vous n'avez normalement rien à faire ici)
+######normalement c'est okay
--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
 
 // Fonction appelée lors du click du bouton
-function start() {
-  // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
-  // Appel de la fonction fetchTodayForecast
+function start(city) {
+    console.log("City input : ", city)
 
-  apiWeather
-    .fetchTodayForecast()
-    .then(function(response) {
-      // Récupère la donnée d'une API
-      const data = response.data;
+    // Création de l'objet apiWeather
+    const apiWeather = new API_WEATHER(city);
+    // Appel de la fonction fetchTodayForecast
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+    apiWeather
+        .fetchTodayForecast()
 
-      // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
-    })
-    .catch(function(error) {
-      // Affiche une erreur
-      console.error(error);
-    });
+        .then(function (response) {
+            // Récupère la donnée d'une API
+            const data = response.data.list;
+
+            for (let i = 0; i < 4; i++) {
+                // On récupère l'information principal
+                const main = data[i].weather[0].main;
+                const description = data[i].weather[0].description;
+                const temp = data[i].temp.day;
+                const icon = apiWeather.getHTMLElementFromIcon(data[i].weather[0].icon);
+
+                // Modifier le DOM
+                document.getElementById(`day-forecast-nb${i}-main`).innerHTML = main;
+                document.getElementById(`day-forecast-nb${i}-more-info`).innerHTML = description;
+                document.getElementById(`day-icon-weather-container-nb${i}`).innerHTML = icon;
+                document.getElementById(`day-forecast-nb${i}-temp`).innerHTML = `${temp}°C`;
+            }
+        })
+        .catch(function (error) {
+            // Affiche une erreur
+            console.error(error);
+        });
 }
